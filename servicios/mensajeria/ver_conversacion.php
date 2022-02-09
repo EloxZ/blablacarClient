8b0d46de5b4556a293ef64df09ea917c262ee0ff
AaronHDN <62114009+AaronHDN@users.noreply.github.com>
2022-02-09 19:47:50 +0100

    a

--- servicios/mensajeria/ver_conversacion.php
+++ servicios/mensajeria/ver_conversacion.php
@@ -39,11 +39,10 @@
         $dataUser = json_decode($resUser);
         var_dump($dataUser);
     }
-
+    include "../../includes/header.php";
 ?>
 
-<head><link rel="stylesheet" href="../css/styles.css"></head>
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
 <h1 align="center">Conversación con <?php echo $dataUser->data->usuario[0]->email;?></h1>
 
 <table style="width:60%" align="center">
