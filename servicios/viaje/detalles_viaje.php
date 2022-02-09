53017d8596c16db682cf3aabbaedb0fdda6b2325
Sergio M <uo271563@uniovi.es>
2022-02-09 02:05:05 +0100

    aaaaaaaaaa

--- servicios/viaje/detalles_viaje.php
+++ servicios/viaje/detalles_viaje.php
@@ -4,7 +4,7 @@
     $viaje = $dataTravel->data->viaje[0];
     var_dump($dataTravel);
     
-    $resConductor = file_get_contents("https://blablacariw.herokuapp.com/users/".$viaje->id_conductor);
+    $resConductor = file_get_contents("https://blablacariw.herokuapp.com/users/".$dataTravel->data->viajes[0]->id_conductor);
     $dataConductor = json_decode($resConductor);
     $conductor = $dataConductor->data->usuarios[0];
 
