28046a71f05e703713eebdc141a22b6ec60f51f0
AaronHDN <62114009+AaronHDN@users.noreply.github.com>
2022-02-09 19:45:42 +0100

    aa

--- servicios/mensajeria/ver_conversacion.php
+++ servicios/mensajeria/ver_conversacion.php
@@ -42,7 +42,7 @@
 
 ?>
 
-<head><link rel="stylesheet" href="../../css/styles.css"></head>
+<head><link rel="stylesheet" href="../css/styles.css"></head>
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 <h1 align="center">Conversación con <?php echo $dataUser->data->usuario[0]->email;?></h1>
 
