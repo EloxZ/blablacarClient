29348636cdf656ac095c979f657e6d6edf1aee2b
AaronHDN <62114009+AaronHDN@users.noreply.github.com>
2022-02-10 21:35:45 +0100

    a

--- servicios/viaje/detalles_viaje.php
+++ servicios/viaje/detalles_viaje.php
@@ -1,7 +1,4 @@
 <?php 
-    session_start();
-    $user = (array) $_SESSION['usuario'];
-    var_dump($user);
 
     $resTravel = file_get_contents("https://blablacariw.herokuapp.com/travels/".$_GET['id']);
     $dataTravel = json_decode($resTravel);
